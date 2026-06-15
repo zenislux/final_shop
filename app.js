bcaf91c6c8e230070d6db0ec81d21a73ed4116d9
zenislux <zenith47d@gmail.com>
2026-06-15 16:44:45 +0900

    fix: 상대경로 수정본

--- app.js
+++ app.js
@@ -104,13 +104,14 @@ initDb.serialize(() => {
       // 3. public/images 폴더 내 파일명과 매칭되는 오리지널 8종 라인업 데이터
       // [name, description, price, emoji, image, likes, is_featured]
       const fruits = [
-        ['싱싱한 사과', '아침에 먹으면 황금 같은 달콤한 국산 사과', 3000, '🍎', 'original/apple.png', 12, 1],
-        ['부드러운 바나나', '당도 최고! 든든한 다이어트 친구 필리핀 바나나', 4500, '🍌', 'original/banana.png', 8, 1], // 💡 featured_product에 다 나오게 1로 변경!
-        ['상큼한 포도', '한 알씩 쏙쏙 빼먹는 재미가 있는 달달한 캠벨 포도', 7000, '🍇', 'original/grape.png', 15, 1],
-        ['새콤달콤 키위', '비타민C가 풍부해 피로 해소에 좋은 뉴질랜드 골드키위', 3500, '🥝', 'original/kiwi.png', 5, 1],
-        ['싱그러운 오렌지', '과즙이 팡팡 터지는 고당도 캘리포니아 오렌지', 2500, '🍊', 'original/orange.png', 9, 1],
-        ['달콤한 복숭아', '풍부한 과즙과 은은한 향이 일품인 부드러운 백도 복숭아', 5500, '🍑', 'original/peach.png', 21, 1],
-        ['시원한 수박', '여름철 더위를 한방에 날려줄 속이 꽉 찬 당도 선별 수박', 18000, '🍉', 'original/watermelon.png', 34, 1]
+        // 💡 파일명 앞에 'images/'를 직접 붙여서 DB에 저장합니다!
+        ['싱싱한 사과', '아침에 먹으면 황금 같은 달콤한 국산 사과', 3000, '🍎', 'images/original/apple.png', 12, 1],
+        ['부드러운 바나나', '당도 최고! 든든한 다이어트 친구 필리핀 바나나', 4500, '🍌', 'images/original/banana.png', 8, 1],
+        ['상큼한 포도', '한 알씩 쏙쏙 빼먹는 재미가 있는 달달한 캠벨 포도', 7000, '🍇', 'images/original/grape.png', 15, 1],
+        ['새콤달콤 키위', '비타민C가 풍부해 피로 해소에 좋은 뉴질랜드 골드키위', 3500, '🥝', 'images/original/kiwi.png', 5, 1],
+        ['싱그러운 오렌지', '과즙이 팡팡 터지는 고당도 캘리포니아 오렌지', 2500, '🍊', 'images/original/orange.png', 9, 1],
+        ['달콤한 복숭아', '풍부한 과즙과 은은한 향이 일품인 부드러운 백도 복숭아', 5500, '🍑', 'images/original/peach.png', 21, 1],
+        ['시원한 수박', '여름철 더위를 한방에 날려줄 속이 꽉 찬 당도 선별 수박', 18000, '🍉', 'images/original/watermelon.png', 34, 1]
       ];
 
       const stmt = initDb.prepare('INSERT INTO products (name, description, price, emoji, image, likes, is_featured) VALUES (?, ?, ?, ?, ?, ?, ?)');
